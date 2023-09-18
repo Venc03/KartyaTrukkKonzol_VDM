517ce79c3d070a5798835e3925879ae69df22acb
Venc03 <vegi.dani1@gmail.com>
2023-09-18 15:35:39 +0200

    lap kesz

--- src/kartyatrukkkonzolon/Lap.java
+++ src/kartyatrukkkonzolon/Lap.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package kartyatrukkkonzolon;
 
-/**
- *
- * @author vegi.d.mark
- */
 public class Lap {
-    
+
+    private String leiras;
+
+    public Lap(String leiras) {
+        this.leiras = leiras;
+    }
+
+    public String getLeiras() {
+        return leiras;
+    }
+
 }
