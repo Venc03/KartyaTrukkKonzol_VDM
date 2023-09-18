12b8b77dd1c9f6c4be6aad93451f1d827737d736
Venc03 <vegi.dani1@gmail.com>
2023-09-18 15:36:34 +0200

    pakli feltolt athelyezes

--- src/kartyatrukkkonzolon/Pakli.java
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package kartyatrukkkonzolon;
-
-/**
- *
- * @author vegi.d.mark
- */
 public class Pakli {
+    
+    public static String[] pakli = new String[22];
+    
+    Lap lap = new Lap();
+    
+    public void feltolt(String leiras) {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int i = 1;
+        for (String szin : szinek) {
+            for (int e = 0; e < ertekek.length && i < pakli.length; e++) {
+                pakli[i++] = szin + "_" + ertekek[e];
+            }
+        }
+
+    }
     
 }
